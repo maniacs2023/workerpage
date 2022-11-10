83001153716fb864f67b7897c2a29bb8fa6fcdf8
s-sudipta <sudipta.saha1532@gmail.com>
2022-11-10 19:55:07 +0530

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,53 @@
-import '../styles/globals.css'
+//https://youtu.be/ZmpO65DhRN0 
+//https://github.com/sairajchouhan/nextjs-firebase-auth/blob/main/pages/_app.tsx
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../styles/globals.css';
+import Head from "next/head";
 import type { AppProps } from 'next/app'
+import Footer from "../component/footer/footer.jsx"
+import Navbar from "../component/navbar/navbar.jsx"
+import { AuthContextProvider } from '../context/AuthContext'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import { parseCookies } from 'nookies';
+import ProtectedRoute from '../context/ProtectedRoute'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const noAuthRequired = ['/', '/login', '/signup', '/search']
+
+function MyApp({ Component, pageProps }: AppProps) {
+const router = useRouter()
+const cookie = parseCookies()
+useEffect(()=>{
+    if(cookie.theme == 'dark'){
+      document.documentElement.style.setProperty('--back-gd-color', '#1f2522');
+      document.documentElement.style.setProperty('--text-color', '#cdcdcd');
+      document.documentElement.style.setProperty('--second-bg-color', '#222220');
+    }
+  else{
+    document.documentElement.style.setProperty('--back-gd-color', '#fff');
+    document.documentElement.style.setProperty('--text-color', '#212529');
+    document.documentElement.style.setProperty('--second-bg-color', ' #f0f7ff ');
+  }
+},[cookie.theme]);  
+return (
+    <AuthContextProvider>
+      <Head>
+        <title>Miniacs</title>
+        <link rel="icon" href="/icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      
+      <Navbar/>
+      {noAuthRequired.includes(router.pathname) ? (
+        <div className="main"><Component {...pageProps} /></div>
+      ) : (
+        <ProtectedRoute>
+          <div className="main"><Component {...pageProps} /></div>
+        </ProtectedRoute>
+      )}
+      <Footer/>
+    </AuthContextProvider>
+  )
 }
+export default MyApp
